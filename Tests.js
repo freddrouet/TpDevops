96262cba8520c82a56f87e7f82fa93d8665c4e8c
fred <fredericd@bleutec.fr>
2023-02-08 15:22:37 +0100

    Ajout des tests

---
+++ Tests.js
@@ -0,0 +1,26 @@
+import { moteur, portes, vitesse, poids } from "./module";
+
+describe("moteur function", () => {
+  it("doit retourner 1200", () => {
+    expect(moteur()).toBe("1200");
+  });
+});
+
+describe("portes function", () => {
+  it("doit retourner 4", () => {
+    expect(portes()).toBe(4);
+  });
+});
+
+describe("vitesse function", () => {
+  it("doit retourner 100", () => {
+    expect(vitesse()).toBe("100");
+  });
+});
+
+describe("poids function", () => {
+  it("doit retourner 900", () => {
+    expect(poids()).toBe("900");
+  });
+});
+tes;
--- voitures.js
+++ voitures.js
@@ -2,8 +2,16 @@ export function moteur() {
   moteur === "1200";
 }
 export function portes() {
-  portes === "4";
+  portesAvg === "1";
+  portesAvd === "1";
+  portesArg === "1";
+  portesArd === "1";
+
+  nombreDePortes == portesAvg + portesAvd + portesArg + portesArd;
 }
 export function vitesse() {
   vitesse === "100";
+}
+export function poids() {
+  poids === "900";
 }
